87fb7023e9966b60916275e1fb9234e664c22a85
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2022-12-09 21:50:18 +0100

    Update

---
+++ PresentationTier/HttpClients/ClientImpl/OrderHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.ClientImpl;
+
+public class OrderHttpClient : IOrderService
+{
+    private readonly HttpClient httpClient;
+
+    public OrderHttpClient(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task RegisterOrderAsync(string username)
+    {
+        // HttpResponseMessage response = await httpClient.PostAsync($"Order/order?username={username}",);
+    }
+
+    public async Task RegisterOrderItemAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PresentationTier/HttpClients/ClientInterfaces/IOrderService.cs
@@ -0,0 +1,8 @@
+﻿namespace HttpClients.ClientInterfaces;
+
+public interface IOrderService
+{
+    public Task RegisterOrderAsync(string username);
+
+    public Task RegisterOrderItemAsync(string username);
+}
