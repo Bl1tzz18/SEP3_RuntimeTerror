15da78c123fa5a60676bbf5e6a75cb61f6487ced
ZoT1YaK <robert.zustars@gmail.com>
2022-12-14 07:48:07 +0100

    Last Update

--- PresentationTier/BlazorApp/Program.cs
+++ PresentationTier/BlazorApp/Program.cs
@@ -16,7 +16,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7129") });
 
 
-builder.Services.AddScoped<IUserService, UserHttpClient>();
 builder.Services.AddScoped<IProductService, ProductHttpClient>();
 builder.Services.AddScoped<IOrderService, OrderHttpClient>();
 
